836d0dfe708bbfec436e26f13e6c631d3bce395b
mlopez383 <mlopez383@gmail.com>
2020-05-09 22:02:33 -0400

    Use of PathVariable in GET Controller

--- src/main/java/com/lopezmiguelangel/springboot/basics/example/BooksController.java
+++ src/main/java/com/lopezmiguelangel/springboot/basics/example/BooksController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,9 @@ public class BooksController {
 		return Arrays.asList(
 				new Book(1l, "Java", "Deitel"));
 	}
-	
+
+	@GetMapping("/book/{id}")
+	public Book getBook(@PathVariable long id){
+		return new Book(id, "Java", "Deitel");
+	}
 }
